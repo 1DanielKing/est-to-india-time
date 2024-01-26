85b244c26f48756e47421c4534f51cc3ab1f0d4e
Daniel King <danielking4684@gmail.com>
2024-01-25 20:49:11 -0500

    partial hours logic

--- src/App.java
+++ src/App.java
@@ -3,20 +3,32 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String ampm24;
         System.out.print("\nThis app will do a simple mathematical process to change time from EST to India.\n");
-        System.out.print("Please enter the EST time in the format hh:mm\n");
-        String userInput = scanner.next();
-        System.out.print("AM, PM, or 24 hour clock? Enter A/P/2:");
-        if(ampm24 != "A" || "P" || "2"){System.out.println("Entry error. Please enter as A/P/2" );}
+        System.out.print("Please enter the EST time in the 24-hour clock format hh:mm, \n");
+        String userInput = scanner.next(); // take user input
+        
+        // System.out.print("AM, PM, or 24 hour clock? Enter A/P/2:"); // choose am pm or 24 hour clock
+        // String ampm24 = scanner.next(); //take user input for am pm or 24
+        // if(ampm24.equalsIgnoreCase("A")){System.out.println("Entry error. Please enter as A/P/2" );} //conditional to check for am pm or 24
         
         String[] timeArray = userInput.split(":");
         int inputHours = Integer.parseInt(timeArray[0]);
-        if(inputHours <= 12){
-            boolean twelveHourClock = true;
-        }
         int inputMinutes = Integer.parseInt(timeArray[1]);
         System.out.println("The time you gave is " + inputHours + ":" + inputMinutes );
+
+
+        // hours logic - will need minutes aspect added
+        int hoursChanging = inputHours + 10;
+        int resultHours;
+        if(hoursChanging >= 24){
+            resultHours = hoursChanging - 24;
+        }
+        else{
+            resultHours = inputHours + 10;
+        }
+
+        System.out.println(resultHours);
+        
 
         // we need to take hours and add 10. but IF it passes 12 this will cause confusion
         // perhaps we need to determine if hours given is 24 hour clock or 12 hour clock am/pm
