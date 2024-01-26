2c7b97618eef601d776ffda69af1d0a8af3ec906
Daniel King <danielking4684@gmail.com>
2024-01-25 21:23:37 -0500

    print time as 21:09 instead of 21:9

--- src/App.java
+++ src/App.java
@@ -7,6 +7,7 @@ public class App {
         System.out.print("Please enter the EST time in the 24-hour clock format hh:mm, \n");
         String userInput = scanner.next(); // take user input
         
+        
         // System.out.print("AM, PM, or 24 hour clock? Enter A/P/2:"); // choose am pm or 24 hour clock
         // String ampm24 = scanner.next(); //take user input for am pm or 24
         // if(ampm24.equalsIgnoreCase("A")){System.out.println("Entry error. Please enter as A/P/2" );} //conditional to check for am pm or 24
@@ -17,7 +18,16 @@ public class App {
         System.out.println("The time you gave is " + inputHours + ":" + inputMinutes );
 
 
-        // hours logic - will need minutes aspect added
+        // minutes logic... when minutes > 60, increase hours by +1 and minutes - 60
+        int minutesChanging = inputMinutes + 30;
+        int resultMinutes;
+        if(minutesChanging >= 60){
+            resultMinutes = minutesChanging - 60;
+            inputHours = inputHours + 1;
+        }
+        else resultMinutes = minutesChanging;
+
+        // hours logic
         int hoursChanging = inputHours + 10;
         int resultHours;
         if(hoursChanging >= 24){
@@ -27,16 +37,13 @@ public class App {
             resultHours = inputHours + 10;
         }
 
-        System.out.println(resultHours);
-        
-
-        // we need to take hours and add 10. but IF it passes 12 this will cause confusion
-        // perhaps we need to determine if hours given is 24 hour clock or 12 hour clock am/pm
-        // we need to add 30 minutes. however after 60 we need to add to hours 
-        // and add remainders to next minutes
-
+        // add a 0 in front of single digit minutes when printing result
+        if(resultMinutes < 10)System.out.println(resultHours + ":" + "0" + resultMinutes);    
+        else System.out.println(resultHours + ":" + resultMinutes);
 
         scanner.close();
     }
 }
 
+//can even input clock and control execution
+//Thread.sleep(1000);
---
+++ src/timeChangeLogic.java
@@ -0,0 +1,5 @@
+public class timeChangeLogic {
+    
+}
+// can move 24 hour clock logic here
+// re-use 24 hour clock logic for all inputs am pm or 24 hour just convert
